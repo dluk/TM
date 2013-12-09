fce69e5b304ef60582aba0e4ae27e4068f29a20d
Gediminas <gediminas.kucas@gmail.com>
2013-12-09 22:41:06 +0200

    bbs

---
+++ TransliavimoMetodai/ClassLibrary1/LeksemuAnalizatorius.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using TM.LeksinisAnalizatorius;
+
+namespace TM.SintaksinisAnalizatorius
+{
+    public interface ILeksemuAnalizatorius
+    {
+        List<string> Leksema { get; set; }
+
+        List<LentelesLeksema> VarduLentele { get; set; }
+
+        string Analyze(int zodis);
+
+    }
+
+    public class LeksemuAnalizatorius : ILeksemuAnalizatorius
+    {
+        public List<string> Leksema { get; set; }
+
+        public List<LentelesLeksema> VarduLentele { get; set; }
+
+        public LeksemuAnalizatorius(List<LentelesLeksema> VarduLentele)
+        {
+            this.VarduLentele = VarduLentele;
+        }
+
+        public LeksemuAnalizatorius()
+        {
+        }
+
+        public string Analyze(int zodis)
+        {
+            return "";
+        }
+    }
+
+    public class DeklaravimoAnal : LeksemuAnalizatorius
+    {
+
+        public List<ILeksemuAnalizatorius> DeklaravimoLeksemos = new List<ILeksemuAnalizatorius>()
+        {
+            new MasyvoDeklaravimoAnal(),
+            new FunkcijosDeklaravimoAnal(),
+            new KintamojoDeklaravimoAnal()
+        };
+
+        public new string Analyze(int zodis)
+        {
+            while(VarduLentele[zodis].Reiksme != "begin")
+            {
+                foreach (var deklaravimoLeksema in DeklaravimoLeksemos)
+                {
+                    if (deklaravimoLeksema.Leksema.Contains(VarduLentele[zodis].Reiksme))
+                    {
+                        deklaravimoLeksema.Analyze(zodis);
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+    public class MasyvoDeklaravimoAnal : LeksemuAnalizatorius
+    {
+        public MasyvoDeklaravimoAnal()
+        {
+            Leksema = new List<string>() {"array"};
+        }
+
+        public new string Analyze(int zodis)
+        {
+            return "";
+        }
+
+    }
+
+    public class FunkcijosDeklaravimoAnal : LeksemuAnalizatorius
+    {
+        public FunkcijosDeklaravimoAnal()
+        {
+            Leksema = new List<string>() { "function" };
+        }
+
+        public new string Analyze(int zodis)
+        {
+            return "";
+        }
+
+    }
+
+    public class KintamojoDeklaravimoAnal : LeksemuAnalizatorius
+    {
+        public KintamojoDeklaravimoAnal()
+        {
+            Leksema = new List<string>() {"string", "int", "dec"};
+        }
+
+        public new string Analyze(int zodis)
+        {   
+            TipasAnal tipasAnal = new TipasAnal();
+            return tipasAnal.Analyze(zodis);
+        }
+
+    }
+
+    public class TipasAnal : LeksemuAnalizatorius
+    {
+        public TipasAnal()
+        {
+            Leksema = new List<string>(){"string","int","dec"};
+        }
+
+        public new string Analyze(int zodis)
+        {
+            if (Leksema.IndexOf(VarduLentele[zodis].Reiksme) == 0)
+            {
+                return zodis + ":neegzistuojantis tipas";
+            }
+            else
+            {
+                return "";
+            }
+        }
+    }
+}
---
+++ TransliavimoMetodai/ClassLibrary1/SintaksinisAnalizatorius.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using TM.LeksinisAnalizatorius;
+
+namespace TM.SintaksinisAnalizatorius
+{
+    public class SintaksinisAnalizatorius
+    {
+        public List<LentelesLeksema> VarduLentele;
+
+        public SintaksinisAnalizatorius(List<LentelesLeksema> varduLentele)
+        {
+            this.VarduLentele = varduLentele;
+        }
+
+        public void Analizuoti()
+        {
+            if (VarduLentele.First().Reiksme != "begin")
+            {
+
+            }
+        }
+    }
+
+}
